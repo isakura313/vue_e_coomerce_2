4dd5aeefcd8f225c821f51296a586cdb974c0a7e
Paul <isakura313@gmail.com>
2020-11-19 20:10:42 +0300

    adsasd

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,9 +1,12 @@
 <template>
-<div class="column is-one-quarter">
-<div class="card">
-
-</div>
-</div>
+  <div class="column is-one-quarter">
+    <div class="card">
+      <img src="" class="card__img">
+      <h3 class="card__title"> Заголовок товара</h3>
+      <p class="card__price">1 000 Р</p>
+      <button class="is-danger is-pulled-right">Заказать</button>
+    </div>
+  </div>
 </template>
 
 <script>
