15c100449d6e6f8c0b955e21664b981b59ea1b9d
Paul <isakura313@gmail.com>
2020-11-19 19:59:47 +0300

    first commit

---
+++ src/components/Card.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="column is-one-quarter">
+<div class="card">
+
+</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Card.vue',
+};
+</script>
+
+<style scoped>
+
+</style>
